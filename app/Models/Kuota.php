4e1eb4828b4fb3dc8352318a06911061ea534cac
dekstop <aflah24ti@mahasiswa.pcr.ac.id>
2026-07-13 20:50:51 +0700

    Kuota Laundry

--- app/Models/Kuota.php
+++ app/Models/Kuota.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kuota extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['max_quota'];
 }
